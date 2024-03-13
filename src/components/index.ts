05c631822b87f4a6de8fa69a0ae62522a00e8d10
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2024-03-13 12:32:47 +0500

    11

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import NavbarComponent from "./ui/Navbar/navbar-component";
 import { ThemeSwitcher } from "./ui/ThemeSwitch/theme-switcher";
 import Sidebar from "./ui/Sidebar/Sidebar";
-import CustomLoading from "./ui/Loading/CustomLoading";
+import CustomLoading from "./ui/Loading/custom-loading";
 
 export { NavbarComponent, ThemeSwitcher, Sidebar, CustomLoading };
