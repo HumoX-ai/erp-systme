ce62ee8845ad127781d265431e07f842c3b5cdb1
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2024-03-13 12:26:20 +0500

    8

--- src/components/ui/Loading/Loading.tsx
+++ src/components/ui/Loading/Loading.tsx
@@ -1,30 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Spinner } from "@nextui-org/react";
-import { CustomLoadingType } from "./types";
 
-const CustomLoading = (props: CustomLoadingType) => {
-  const {
-    loading,
-    children,
-    spinnerClass,
-    className,
-    asElement: Component,
-  } = props;
-
-  return loading ? (
-    <Component
-      className={`flex items-center justify-center h-full ${className}`}
-    >
-      <Spinner className={spinnerClass} color="white" />
-    </Component>
-  ) : (
-    <>{children}</>
+const CustomLoading = () => {
+  return (
+    <div>
+      <Spinner
+        className="h-[90vh] flex items-center justify-center"
+        color="white"
+      />
+    </div>
   );
-};
-
-CustomLoading.defaultProps = {
-  loading: false,
-  asElement: "div",
 };
 
 export default CustomLoading;
