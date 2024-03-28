278fa03b322db2292df8af206f9e16d00baec446
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2024-03-28 14:05:46 +0500

    fix scheme error

--- src/modules/filials/scheme/index.ts
+++ src/modules/filials/scheme/index.ts
@@ -4,5 +4,4 @@ export const validationSchema = Yup.object().shape({
   filialName: Yup.string().required("Filial kiriting"),
   address: Yup.string().required("Manzil kiriting"),
   phone: Yup.string().required("Telefon raqam kiriting"),
-  quantity: Yup.number().required("Mahsulot soni kiriting"),
 });
